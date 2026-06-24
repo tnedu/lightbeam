9895b99bb39cb67502241f25622f1b430a28e84e
thanuja bachala <thanuja.bachala@tnedu.gov>
2026-06-24 14:09:15 -0500

    delete debug statement

--- lightbeam/delete.py
+++ lightbeam/delete.py
@@ -40,9 +40,9 @@ class Deleter:
             self.logger.info("finished processing endpoint {0}!".format(endpoint))
             self.logger.info("  (final status counts: {0})".format(self.lightbeam.status_counts))
             self.lightbeam.log_status_reasons()
-
         # write structured output (if needed)
         self.lightbeam.write_structured_output("delete")
+        print("Newly added I am here msg - finished deleting data for {0} endpoints!".format(len(endpoints)))
 
     # Deletes data matching payloads in config.data_dir for single endpoint
     async def do_deletes(self, endpoint):
