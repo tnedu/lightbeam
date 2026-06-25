12f640398c1567e44734c35ec4cec60677255eff
thanuja bachala <thanuja.bachala@tnedu.gov>
2026-06-25 03:13:37 -0500

    File is being empty so adding a log instead

--- lightbeam/delete.py
+++ lightbeam/delete.py
@@ -39,11 +39,11 @@ class Deleter:
             asyncio.run(self.do_deletes(endpoint))
             self.logger.info("finished processing endpoint {0}!".format(endpoint))
             self.logger.info("  (final status counts: {0})".format(self.lightbeam.status_counts))
+            self.logger.info("Status Count Message forendpoint {0}!".format(endpoint), "final status counts: {0}".format(self.lightbeam.status_counts))
             self.lightbeam.log_status_reasons()
         # write structured output (if needed)
-        print("Newly added I am here msg - before calling structured output")
         self.lightbeam.write_structured_output("delete")
-        print("Newly added I am here msg - finished deleting data for {0} endpoints!".format(len(endpoints)))
+
 
     # Deletes data matching payloads in config.data_dir for single endpoint
     async def do_deletes(self, endpoint):
